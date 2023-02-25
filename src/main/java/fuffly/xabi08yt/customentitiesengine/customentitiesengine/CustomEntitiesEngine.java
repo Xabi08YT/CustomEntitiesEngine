36cec7452f9b93ac08ca60868a60e4dc5592c778
Xabi08YT <xabigoity@gmail.com>
2023-02-25 21:45:44 +0100

    Added Hello World at startup Added See ya at Shutdown

--- src/main/java/fuffly/xabi08yt/customentitiesengine/customentitiesengine/CustomEntitiesEngine.java
+++ src/main/java/fuffly/xabi08yt/customentitiesengine/customentitiesengine/CustomEntitiesEngine.java
@@ -7,11 +7,12 @@ public final class CustomEntitiesEngine extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        System.out.println("Hello World");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        System.out.println("See ya !");
     }
 }
