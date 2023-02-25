45aa1b19cc3d35780c78e8986af27af2092cf016
Xabi08YT <xabigoity@gmail.com>
2023-02-25 21:35:56 +0100

    Initial Commit

---
+++ src/main/java/fuffly/xabi08yt/customentitiesengine/customentitiesengine/CustomEntitiesEngine.java
@@ -0,0 +1,17 @@
+package fuffly.xabi08yt.customentitiesengine.customentitiesengine;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CustomEntitiesEngine extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
